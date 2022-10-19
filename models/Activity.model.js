a73b4388c81659482fc1a2787827a745e3642e0f
Guillermo Cubells <guillermocubells@gmail.com>
2022-10-19 18:51:48 +0200

    feature: mood model draft

---
+++ models/Activity.model.js
@@ -0,0 +1,4 @@
+const { Schema, model } = require("mongoose");
+const { userCollectionName } = require("./User.model");
+
+const activitySchema = new Schema({});
---
+++ models/Mood.model.js
@@ -0,0 +1,109 @@
+const { Schema, model } = require("mongoose");
+const { userCollectionName } = require("./User.model");
+
+const moodSchema = new Schema(
+  {
+    status: {
+      type: String,
+      required: ["Awful", "Bad", "Okay", "Good", "Great"],
+      properties: {
+        Awful: {
+          enum: [
+            "Stressed",
+            "Lonely",
+            "Annoyed",
+            "Dissapointed",
+            "Anxious",
+            "Sad",
+            "Hurt",
+            "Angry",
+          ],
+        },
+        Bad: {
+          enum: [
+            "Bored",
+            "Stressed",
+            "Annoyed",
+            "Anxious",
+            "Tired",
+            "Lonely",
+            "Dissapointed",
+            "Sad",
+          ],
+        },
+        Okay: {
+          enum: [
+            "Hopeful",
+            "Content",
+            "Distracted",
+            "Tired",
+            "Calm",
+            "Confused",
+            "Bored",
+            "Stressed",
+          ],
+        },
+        Good: {
+          enum: [
+            "Happy",
+            "Grateful",
+            "Relaxed",
+            "Calm",
+            "Excited",
+            "Confident",
+            "Hopeful",
+            "Content",
+          ],
+        },
+        Great: {
+          enum: [
+            "Blessed",
+            "Happy",
+            "Grateful",
+            "Relaxed",
+            "Inspired",
+            "Excited",
+            "Confident",
+            "Hopeful",
+          ],
+        },
+      },
+    },
+    activity: {
+      type: String,
+      enum: [
+        "Family",
+        "Friends",
+        "Food",
+        "Relationships",
+        "Healthy",
+        "Exercise",
+        "Spirituality",
+        "Movies",
+        "Reading",
+        "Gaming",
+        "Shopping",
+        "Travel",
+        "Work",
+        "Sleep",
+        "Studies",
+        "Finances",
+      ],
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    // images:
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: userCollectionName,
+    },
+  },
+  { timestamps: true }
+);
+
+const Mood = model("Mood", moodSchema);
+
+module.exports = Mood;
