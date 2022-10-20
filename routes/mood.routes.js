00358e9aa48478bbdb9d61dbde856ca6463c7b07
poncho678 <vincent.tollens@googlemail.com>
2022-10-20 22:21:43 +0200

    started to work on mood routes

--- routes/mood.routes.js
+++ routes/mood.routes.js
@@ -1,14 +1,30 @@
 const express = require("express");
-
+const isLoggedIn = require("../middleware/isLoggedIn");
 const moodRouter = express.Router();
+
+const Mood = require("../models/Mood.model");
+
+moodRouter.use(isLoggedIn);
 
 // view mood
 moodRouter.get("/:slug", (req, res) => {
+  const { user } = req;
   const { slug } = req.params;
-  res.json({ hi: "hi" });
+  res.json({ hi: `${slug}` });
 });
 
 // create mood
-moodRouter.post("/create", (req, res) => {});
+moodRouter.post("/create", (req, res) => {
+  //   get the user
+  const { user } = req;
+  console.log(user);
+
+  // get values from frontend
+  const { status, properties, activity, description, date, image } = req.body;
+
+  //   do some validation on the form
+
+  //   create new mood
+});
 
 module.exports = moodRouter;
