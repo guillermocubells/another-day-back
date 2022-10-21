e9b8494ba65057dc0fbfbe6b5d2acba9b6309ab0
poncho678 <vincent.tollens@googlemail.com>
2022-10-21 10:40:24 +0200

    created mood enum in separate file

---
+++ utils/mood-enum-data.js
@@ -0,0 +1,85 @@
+const MOOD_STATUS_ENUM = ["Awful", "Bad", "Okay", "Good", "Great"];
+
+const MOOD_SUBSTATUS_AWFUL = [
+  "Stressed",
+  "Lonely",
+  "Annoyed",
+  "Dissapointed",
+  "Anxious",
+  "Sad",
+  "Hurt",
+  "Angry",
+];
+
+const MOOD_SUBSTATUS_BAD = [
+  "Bored",
+  "Stressed",
+  "Annoyed",
+  "Anxious",
+  "Tired",
+  "Lonely",
+  "Dissapointed",
+  "Sad",
+];
+
+const MOOD_SUBSTATUS_OKAY = [
+  "Hopeful",
+  "Content",
+  "Distracted",
+  "Tired",
+  "Calm",
+  "Confused",
+  "Bored",
+  "Stressed",
+];
+
+const MOOD_SUBSTATUS_GOOD = [
+  "Happy",
+  "Grateful",
+  "Relaxed",
+  "Calm",
+  "Excited",
+  "Confident",
+  "Hopeful",
+  "Content",
+];
+
+const MOOD_SUBSTATUS_GREAT = [
+  "Blessed",
+  "Happy",
+  "Grateful",
+  "Relaxed",
+  "Inspired",
+  "Excited",
+  "Confident",
+  "Hopeful",
+];
+
+const MOOD_ACTIVITIES_ENUM = [
+  "Family",
+  "Friends",
+  "Food",
+  "Relationships",
+  "Healthy",
+  "Exercise",
+  "Spirituality",
+  "Movies",
+  "Reading",
+  "Gaming",
+  "Shopping",
+  "Travel",
+  "Work",
+  "Sleep",
+  "Studies",
+  "Finances",
+];
+
+module.exports = {
+  MOOD_STATUS_ENUM,
+  MOOD_SUBSTATUS_AWFUL,
+  MOOD_SUBSTATUS_BAD,
+  MOOD_SUBSTATUS_OKAY,
+  MOOD_SUBSTATUS_GOOD,
+  MOOD_SUBSTATUS_GREAT,
+  MOOD_ACTIVITIES_ENUM,
+};
