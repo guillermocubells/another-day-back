e0f0b159c48dcfbd63d40221581bc722a97f6bd7
poncho678 <vincent.tollens@googlemail.com>
2022-10-21 21:08:45 +0200

    sending substatus to homepage

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,10 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const { ACTIVITIES } = require("../utils/activity-data");
-const { MOOD_STATUS_ENUM } = require("../utils/mood-enum-data");
+const { MOOD_STATUS_ENUM, MOOD_SUBSTATUS } = require("../utils/mood-enum-data");
 
 router.get("/", (req, res, next) => {
-  res.json({ activities: ACTIVITIES, mood_status: MOOD_STATUS_ENUM });
+  res.json({
+    activities: ACTIVITIES,
+    mood_status: MOOD_STATUS_ENUM,
+    mood_substatus: MOOD_SUBSTATUS,
+  });
 });
 
 module.exports = router;
