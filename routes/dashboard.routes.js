f1420e1d726745dad86caa22020360b551df1a78
Guillermo Cubells <guillermocubells@gmail.com>
2022-10-25 16:28:38 +0200

    feature:dashboard routes working

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const isLoggedIn = require("../middleware/isLoggedIn");
-const { Mood } = require("../models/Mood.model");
+const Mood = require("../models/Mood.model");
 // const { addMonths, isValid } = require("date-fns");
 // const { Mood } = require("../models/Mood.model");
 const dashboardRouter = express.Router();
@@ -11,5 +11,21 @@ dashboardRouter.get("/", isLoggedIn, (req, res) => {
     res.json(allMoods);
   });
 });
+
+// dashboardRouter.get("/dashboard", isLoggedIn, (req, res) => {
+//   const { _id } = req.user;
+//   // Find One Goal, that has this slug: slug, and the owner has this value: _id
+//   // Find One By That the req.user._id is the value for owner
+//   Mood.findOne({ owner: _id }).then((possibleMood) => {
+//     console.log("possibleMood:", possibleMood);
+//     if (!possibleMood) {
+//       return res
+//         .status(YOU_SHALL_NOT_PASS_FOCKERS)
+//         .json({ errorMessage: "Syntax Error" });
+//     }
+
+//     res.json(possibleMood);
+//   });
+// });
 
 module.exports = dashboardRouter;
