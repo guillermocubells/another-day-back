494cc8ae1c95c4e42ac3b0de1714cd9dca14e474
poncho678 <vincent.tollens@googlemail.com>
2022-10-27 21:55:53 +0200

    feat: activities are now fetched from the database

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,10 +4,24 @@ const dashboardRouter = require("./dashboard.routes");
 
 const { ACTIVITIES } = require("../utils/activity-data");
 const { MOOD_STATUS_ENUM, MOOD_SUBSTATUS } = require("../utils/mood-enum-data");
+const Activity = require("../models/Activity.model");
+const isLoggedIn = require("../middleware/isLoggedIn");
 
-router.get("/", (req, res, next) => {
+router.get("/", isLoggedIn, async (req, res, next) => {
+  const { user } = req;
+
+  const allActivities = await Activity.find({
+    $or: [{ custom: false }, { owner: user._id }],
+  })
+    .then((possibleActivities) => {
+      return possibleActivities;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
   res.json({
-    activities: ACTIVITIES,
+    activities: await allActivities,
     mood_status: MOOD_STATUS_ENUM,
     mood_substatus: MOOD_SUBSTATUS,
   });
